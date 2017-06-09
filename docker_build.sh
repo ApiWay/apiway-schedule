6ed976769d91cc0f08b515e3b783a0e6c60485ab
Youngbok Yoon <master@bluehack.net>
2017-06-09 17:42:21 +0900

    Initial

---
+++ docker_build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+SERVICE_NAME="apiway-schedule"
+TAG=$1
+AWS_CONTAINER_REGISTRY="539277938309.dkr.ecr.us-west-2.amazonaws.com"
+
+
+aws ecr get-login --region us-west-2 > ecr_login.sh
+chmod +x ecr_login.sh
+./ecr_login.sh
+
+docker build -t $SERVICE_NAME:$TAG .
+docker tag $SERVICE_NAME:$TAG $AWS_CONTAINER_REGISTRY/$SERVICE_NAME:$TAG
+docker push $AWS_CONTAINER_REGISTRY/$SERVICE_NAME:$TAG
+kubectl set image deployment/$SERVICE_NAME $SERVICE_NAME=$AWS_CONTAINER_REGISTRY/$SERVICE_NAME:$TAG
+kubectl rollout status deployment/$SERVICE_NAME
+kubectl delete pods -l name=$SERVICE_NAME
+
+rm -f ecr_login.sh
